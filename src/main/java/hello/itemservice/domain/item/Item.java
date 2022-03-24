fe274e7d9bd2869e0d85a3476e59251ba00646c1
zidol <ajh3166@naver.com>
2022-03-25 00:52:44 +0900

    Bean Validation 에러코드, 오브젝트 오류, 수정 폼 적용

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -2,12 +2,15 @@ package hello.itemservice.domain.item;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Data
+//기능이 약해서  비추천 컨트롤러에서 자바코드로 하는걸 추천
+//@ScriptAssert(lang = "javascript", script = "_this.price * this.quantity >= 10000", message = "총 합이 10000원 넘게 입력해주세요.")
 public class Item {
 
     private Long id;
@@ -16,7 +19,7 @@ public class Item {
     private String itemName;
 
     @NotNull
-    @Range(min = 1000, max = 10000000)
+    @Range(min = 1000, max = 1000000)
     private Integer price;
 
     @NotNull
